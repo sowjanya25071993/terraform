5c71f528da2daf3ea191dcf176e4d5e6d069ddbb
sowji <jujjurisowjanya@gmail.com>
2024-01-03 22:11:50 +0530

    files

---
+++ count/count.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "web" {
+    #count = 11
+    count = length(var.instance_names)
+  ami           = var.ami_id
+  instance_type = local.instance_type
+  
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
+resource "aws_route53_record" "www" {
+  #count = 11
+  count = length(var.instance_names)
+  zone_id = var.zone_id
+  name    = "${var.instance_names[count.index]}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [local.ip]
+}
