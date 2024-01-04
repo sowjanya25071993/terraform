d81e78f9265fb056dfd425b76d0638b12b6f7e1a
sowji <jujjurisowjanya@gmail.com>
2024-01-04 21:18:10 +0530

    new

---
+++ locals/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_names" {
+    type = list
+    default = ["mongodb" , "redis" , "mysql" , "rabbitmq" , "catalogue", "user", "shipping", "cart", "payment", "dispatch", "web"]
+
+}
+variable "ami_id" {
+    default = "ami-03265a0778a880afb"
+}
+variable "zone_id" {
+    default = "Z0380517BLU5489E8PQ"
+}
+variable "domain_name" {
+    default = "sowjanyaaws.xyz"
+}
